8202e9c070a198f2d85a3f6007a798d462960dda
Eugene <onufriev39@gmail.com>
2020-06-01 03:53:00 +0300

    Подправил инициализацию Retrofit.

--- app/src/main/java/com/example/lesson2/WeatherLoader.java
+++ app/src/main/java/com/example/lesson2/WeatherLoader.java
@@ -17,6 +17,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class WeatherLoader {
 
+    Retrofit retrofit;
+
     private OpenWeather openWeather;
     private cityListener cityListener;
 
@@ -36,14 +38,15 @@ public class WeatherLoader {
     }
 
     private void initRetorfit() {
-        Retrofit retrofit;
-        retrofit = new Retrofit.Builder()
-                .baseUrl("http://api.openweathermap.org/") // Базовая часть
-                // адреса
-                // Конвертер, необходимый для преобразования JSON
-                // в объекты
-                .addConverterFactory(GsonConverterFactory.create()).build();
-        // Создаём объект, при помощи которого будем выполнять запросы
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("http://api.openweathermap.org/") // Базовая часть
+                    // адреса
+                    // Конвертер, необходимый для преобразования JSON
+                    // в объекты
+                    .addConverterFactory(GsonConverterFactory.create()).build();
+            // Создаём объект, при помощи которого будем выполнять запросы
+        }
         openWeather = retrofit.create(OpenWeather.class);
     }
 
@@ -73,7 +76,7 @@ public class WeatherLoader {
 
                     @Override
                     public void onFailure(Call<WeatherRequest> call, Throwable t) {
-                        if(cityListener != null){
+                        if (cityListener != null) {
                             cityListener.negativeAction(connectionsErrorMsg);
                         }
                     }
